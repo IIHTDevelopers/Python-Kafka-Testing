60bde223322fda407beded8957dceb1c21238ac5
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-06-19 15:34:14 +0530

    Delete kafka_app directory

--- kafka_app/__init__.py
+++
@@ -1,5 +0,0 @@
-from .consumers import TransactionConsumer
-from .notification import NotificationService
-from .producers import TransactionProducer
-from .services import TransactionManagementService
-from .transactions import Transaction
